9b564560559120a1439442280eeb24b0e62367a2
WelcomeMM <wmbonga@gmail.com>
2019-09-07 15:19:42 +0100

    Bug with rendering of the home pageContent

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-//jshint esversion:6
+//jshint esversion:8
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
@@ -27,18 +23,57 @@ const postSchema = mongoose.Schema({
 	content: String
 });
 
+const pageContentSchema = mongoose.Schema({
+	name: String,
+	content: String
+});
+
 const Post = mongoose.model("Post", postSchema);
+const PageContent = mongoose.model("pageContent", pageContentSchema);
 
+// const homeStartingContent = new PageContent({
+// 	name: "home",
+// 	content: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
 
+// });
+
+// const aboutContent = new PageContent({
+// 	name: "about",
+// 	content: "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui."
+// });
+
+// const contactContent = new PageContent({
+// 	name: "contact",
+// 	content: "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero."
+// });
+
+// PageContent.insertMany([homeStartingContent, aboutContent, contactContent], (err) => {
+// 	if (err) {console.log(err);}
+// });
 
 app.get("/", (req, res) => {
+
+
+	const homeStartingContent = async function () {
+
+		await PageContent.findOne({name: "home"}, (err, foundContent) => {
+		if (!err) {
+			homeContent =  foundContent.content;
+		} else {
+			return err;
+		}
+		});
+	};
+
+
+	console.log(homeContent);
 
 	Post.find({}, (err, foundPosts) => {
 
 		if (!err) {
 		// 	//render
 			res.render("home", {
-			homeContent: homeStartingContent, 
+			homeContent: homeContent, 
 			posts: foundPosts,
 			
 			});
@@ -48,15 +83,27 @@ app.get("/", (req, res) => {
 		}
 	});
 
-	
 });
 
 app.get("/contact", function (req, res) {
-	res.render("contact", {contactPage : contactContent});
+
+	PageContent.findOne({name: "contact"}, (err, foundContent) => {
+		if (!err) {
+			res.render("contact", {contactPage : foundContent.content});
+		}
+	});
+	
 });
 
 app.get("/about", (req, res) => {
-	res.render("about", {aboutPage: aboutContent});
+	
+	PageContent.findOne({name: "about"}, (err, foundContent) => {
+		if (!err) {
+			res.render("about", {aboutPage: foundContent.content});
+		} else {
+			console.log(err);
+		}
+	});
 });
 
 app.get("/compose", function (req, res) {
